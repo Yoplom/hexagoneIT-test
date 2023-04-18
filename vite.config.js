854bfb38e99c5f88a509994641757f8610e801bf
Yoplom <ni.guillou@gmail.com>
2023-04-18 12:39:38 +0200

    redefine the homepage path

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-project/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 });
 // module.exports = {
 //   publicPath: process.env.NODE_ENV === "production" ? "/hexagone-test/" : "/",
